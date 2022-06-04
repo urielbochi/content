6278b9f07640f211646e1477370df1940b890d86
Uriel Bochi <urielbochi@gmail.com>
2022-06-04 15:08:36 -0300

    alt

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -43,7 +43,7 @@ function Projects() {
           <img
             className="projects__image-config image__img recipes__app"
             src={restaurant}
-            alt="A foto foi tirada a noite, em um local escuro iluminado pelas luzes neon de cor roxa do local. É um bar, em seu topo há uma placa com o nome do bar - The District, abaixo, outra placa escrito Party Bar and Grill. As paredes do bar são feitas de vidro, acima dessas paredes existe uma placa que diz COURT CENTER "
+            alt="Imagem do projeto Recipes App. A foto foi tirada a noite, em um local escuro iluminado pelas luzes neon de cor roxa do local. É um bar, em seu topo há uma placa com o nome do bar - The District, abaixo, outra placa escrito Party Bar and Grill. As paredes do bar são feitas de vidro, acima dessas paredes existe uma placa que diz COURT CENTER "
           />
           <div className="image__overlay recipes__app">
             <h1 className="image__title recipes__app">Recipes App</h1>
@@ -54,6 +54,7 @@ function Projects() {
           <img
             className="projects__image-config image__img tractian__dashboard"
             src={TractianLogo}
+            alt="Imagem do projeto Tractian Dashboard. Desenho com fundo azul onde está presente um operário anotando a situação de uma maquina. Acima de cada máquina existe um sinal vermelho com X ou um sinal verde, vermelho se a máquina está com defeito, verde se estiver em funcionamento. A primeira máquina é marcada com um X, a segunda e a terceira também são marcadas com o sinal verde."
           />
           <div
             className="image__overlay tractian__dashboard"
@@ -67,7 +68,8 @@ function Projects() {
           <img
             className="projects__image-config image__img my__portfolio"
             src={portf}
-          />
+            alt="Imagem bege representando o projeto My Portfolio"
+            />
           <div
             className="image__overlay my__portfolio"
             onClick={(e) => getEvent(e)}
@@ -82,6 +84,7 @@ function Projects() {
           <img
             className="projects__image-config image__img shopping__cart"
             src={shopp}
+            alt="Imagem do projeto Shopping Card, contém sacolas de compra de várias cores"
           />
           <div className="image__overlay shopping__cart">
             <h1 className="image__title shopping__cart">Shopping Cart</h1>
@@ -92,6 +95,7 @@ function Projects() {
           <img
             className="projects__image-config image__img solar__system"
             src={solarSystem}
+            alt="Imagem do projeto Solar System, contém a imagem de um céu estrelado em uma galáxia estranha."
           />
           <div className="image__overlay solar__system">
             <h1 className="image__title solar__system">Solar System</h1>
@@ -102,6 +106,7 @@ function Projects() {
           <img
             className="projects__image-config image__img pixels"
             src={pixel}
+            alt="Imagem do projeto Pixel Art, contém a imagem de uma pintura abstrata, cheia de cores."
           />
           <div className="image__overlay pixels">
             <h1 className="image__title pixels">Pixels</h1>
